49077f251a9823070e3a265fd5e89bcd44744483
Ahmed Adebayo <adebayoh76@gmail.com>
2022-12-19 12:48:56 -0800

    Exceptions

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+        i = 0
+            try:
+                        for index in range(x):
+                                        print("{}".format(my_list[index]), end="")
+                                                    i += 1
+            except IndexError:
+                        pass
+                        print()
+                            return i
