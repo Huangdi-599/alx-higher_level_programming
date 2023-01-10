0a73a1a52413dbe4a618ce964897c33cd2c0262a
Ahmed Adebayo <adebayoh76@gmail.com>
2023-01-10 13:05:15 -0800

    dd

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Function that returns a dictionary description"""
 
+
 def class_to_json(obj):
     """Return the dictionary represntation of a simple data structure."""
     return obj.__dict__
