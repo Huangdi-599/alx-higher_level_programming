843e99089f14ab300b2b7b589ae86a3c7fce9211
Ahmed Adebayo <adebayoh76@gmail.com>
2023-02-16 13:31:40 -0800

    vdf

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,3 @@
--- This script lists all privileges user_0d_1 and user_0d_2 has
--- If localhost is not included, the error message will occur as the script attempts to access the % database(?)
-
-SHOW GRANTS IF NOT EXISTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS IF NOT EXISTS FOR 'user_0d_2'@'localhost';
+-- Lists all privileges of the MySQL users user_0d_1 and user_0d_2
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
